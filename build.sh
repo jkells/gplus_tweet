835430a98e8c570556c79be14dd14b44a4fefffa
Jared Kells <jkells@gmail.com>
2011-07-17 22:20:51 +1000

    Complete build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# This build script is just for my local machine. Hard coded paths, beware...
+
 rm -rf build
 mkdir build
 
 cp -r src/* build
+cp .private/secret_keys.js build/lib/secret_keys.js
+
+#/cygdrive/c/Users/jkells/AppData/Local/Google/Chrome/Application/chrome.exe --pack-extension=`cygpath -w pwd`\\build 
